d60e37094f53e9c6452841ac7121b07c01a6de43
denzelroberts <iamdenzelroberts@gmail.com>
2023-11-29 22:02:19 +0000

    fix

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 for first_digit in range(10):
     for second_digit in range(first_digit + 1, 10):
         if (first_digit * 10 + second_digit) <89 :
-            print("{:02d}, ".format(first_digit * 10 + second_digit), end="")
+            print("{:02d}, ".format(first_digit * 10 + second_digit))
 
         else:
             print("{:02d}".format(first_digit * 10 + second_digit), end="")
