f158c00e15c219e7ce16d5cc1ed144018b52eb7c
denzelroberts <iamdenzelroberts@gmail.com>
2023-11-30 15:04:27 +0000

    password

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,6 +1,9 @@
 def validate_password(password):
-    if len(password) >= 8 and any(char.isupper() for char in password) and any(char.islower() for char in password) and any(char.isdigit() for char in password) and ' ' in password :
+    #check if characters are at least 8
+    if len(password) > 7 and any(char.isupper() for char in password) and any(char.islower() for char in password) and any(char.isdigit() for char in password) and " " not in password:
         return True
     
     else:
-        False
+        return False
+    
+print(validate_password("Password1"))
