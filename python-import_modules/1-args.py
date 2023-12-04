66f9f703addf149b406a85bf0dddc9e36ac4662b
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-04 22:10:54 +0000

    length

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python3
 from sys import argv
 
 if __name__ == "__main__":
-    length = len(argv) - 1
-    if length == 0:
+    length_of_args = len(argv) - 1
+    if length_of_args == 0:
         print("{}".format("0 arguments."))
-    elif length == 1:
+    elif length_of_args == 1:
         print("{}".format("1 argument:"))
         print("1: {}".format(argv[1]))
     else:
-        print("{:d} {}".format(length, "arguments:"))
-        for i in range(1, length + 1):
+        print("{:d} {}".format(length_of_args, "arguments:"))
+        for i in range(1, length_of_args + 1):
             print("{:d}: {}".format(i, argv[i]))
