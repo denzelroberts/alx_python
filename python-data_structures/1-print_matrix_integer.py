48a2e2efe5903e29df6c8fbc6c635c936dc98b7a
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-06 21:08:07 +0000

    check "space"

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,15 +4,18 @@ def print_matrix_integer(matrix=[[]]):
         # Loop over each column in the current row
         for j in range(len(matrix[i])):
             # Print element at row i, column j
-            print("{:d} ".format(matrix[i][j]),end='')
+            if j < len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]), end="")
         # Print a new line after each row
-        print("")
+        print()
     return matrix
 
-# matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
-# ]
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-#print_matrix_integer(matrix)
+print_matrix_integer(matrix)
