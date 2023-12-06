2931e3c6eaa52610c7b46773b411e7657d6c2b71
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-06 20:22:06 +0000

    nospace

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         # Loop over each column in the current row
         for j in range(len(matrix[i])):
             # Print element at row i, column j
-            print("{:d}".format(matrix[i][j]),end=' ')
+            print("{:d}".format(matrix[i][j]),end='')
         # Print a new line after each row
         print()
     return matrix
