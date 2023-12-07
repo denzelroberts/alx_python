d60f5e052ec20260ea6676ebb2000b2ff1e6bf2d
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-07 16:38:53 +0000

    mutiply

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,2 +1,2 @@
-def mutiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda my_list: my_list * number, my_list))
