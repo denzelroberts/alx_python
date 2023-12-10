dee60ac69675458374eb6c55e1a5210a2984d052
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-10 18:51:03 +0000

    catching errors

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+"""This is size of a square"""
+class Square:
+    """class of the size of the square"""
+    def __init__(self, size=0):
+        """
+        The initialization of size
+        """
+        self.__size = size
+        try:
+            size = input(int())
+        except TypeError:
+            print("size must be an integer")
+
+        
+        try:
+            size < 0
+        except ValueError:
+            print("size must be >=0")
