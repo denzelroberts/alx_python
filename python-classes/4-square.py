929da3106f1619355bd78c72ea889f52eb2fcfc8
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-11 20:44:46 +0000

    new line

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -46,5 +46,5 @@ class Square:
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
-                        print("#")
-                print("\n")
+                        print("#",end="")
+                print("")
