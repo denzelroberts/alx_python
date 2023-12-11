7c83b20580d7bda73fc2c9a187f462539bcb6076
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-11 20:35:41 +0000

    whiling

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -45,5 +45,6 @@ class Square:
 
         else:
             for i in range(self.__size):
-                for j in range(self.__size < range):
-                    print("#", end="")
+                for j in range(self.__size):
+                    while range < self.__size:
+                        print("#", end="")
