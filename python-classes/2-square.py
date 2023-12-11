47bd03977e82a6717d142e2cdbaabf06922b4aee
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-11 15:57:32 +0000

    omo we are just checking oo

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+"""This is size of a square"""
+class Square:
+    """class of the size of the square"""
+    def __init__(self, size=0):
+        """
+        The initialization of size
+        """
+        self.__size = size
+
+        if type(size) == int:
+            size
+        else:
+            raise TypeError("size must be an integer")
+        
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+    def area(self):
+        self.__size ** 2
+        return self
