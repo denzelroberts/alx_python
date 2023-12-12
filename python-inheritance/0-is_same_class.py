166b9dbdffef2f4a9369b94d04792c280b2045f7
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-12 19:44:21 +0000

    check mic

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,7 +1,7 @@
 """This is size of a function"""
 def is_same_class(obj, a_class):
     """This is size of a function"""
-    if type(obj) == type(a_class()):
+    if type(obj) == type(a_class):
         return True
     elif isinstance(obj, a_class) == isinstance(obj, a_class):
         return False
