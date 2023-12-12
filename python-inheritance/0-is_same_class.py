e4a59cbdca2a016b31c6568ed2f1fc0ead8f0552
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-12 19:34:27 +0000

    oouuuu i dont believe it

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,13 +2,13 @@
 def is_same_class(obj, a_class):
     """This is size of a function"""
     # return type(obj) == type(a_class())
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) == isinstance(obj, a_class)
 
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
+# a = 1
+# if is_same_class(a, int):
+#     print("{} is an instance of the class {}".format(a, int.__name__))
+# if is_same_class(a, float):
+#     print("{} is an instance of the class {}".format(a, float.__name__))
+# if is_same_class(a, object):
+#     print("{} is an instance of the class {}".format(a, object.__name__))
 
