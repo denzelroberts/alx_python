420f38baf5f42f4a0feb5472524635c537e70078
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-12 18:37:36 +0000

    is it true?

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,14 @@
+"""This is size of a class"""
+class a_class:
+    """This is size of a method"""
+    pass
+
+"""This is size of a class"""
+class obj:
+    """This is size of a method"""
+    pass
+
+def is_same_class(obj, a_class):
+    """This is size of a function"""
+    if obj == a_class:
+        return True
