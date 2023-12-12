7f51c1768384b515f260ba3d5cb0e0d98b7a6564
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-12 19:10:13 +0000

    come on

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -16,10 +16,4 @@ def is_same_class(obj, a_class):
     # elif isinstance(obj, a_class) == False:
     #     return False
     
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
+
