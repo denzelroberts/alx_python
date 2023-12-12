8e6234b45c3fe1476a8deb7b661a1426ced49f97
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-12 19:48:48 +0000

    keep it like this

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,8 +1,7 @@
 """This is size of a function"""
 def is_same_class(obj, a_class):
     """This is size of a function"""
-    if type(obj) == type(a_class()):
-        return True
+    return type(obj) == type(a_class())
     # elif isinstance(obj, a_class) == isinstance(obj, a_class):
     #     return False
 
