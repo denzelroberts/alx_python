7d57beb67b3155ae1768e29249624b3596c3da4c
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-14 17:31:52 +0000

    i thought i did

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,25 @@
+"""This is a class"""
+class BaseGeometry:
+    """These are the attributes"""
+    def area(self):
+        """This is a function"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        
+"""This is a class"""        
+class Rectangle(BaseGeometry):
+    """These are the attributes"""
+    def __init__(self, width, height):
+        """validate that"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        """privatize"""
+        self.__width = width
+        self.__height = height
