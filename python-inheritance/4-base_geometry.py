70aa6ad9b7138c39c06192b85885aa8af27f6cf8
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-14 17:13:04 +0000

    exceptions

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,6 @@
+"""This is a class"""
+class BaseGeometry:
+    """These are the attributes"""
+    def area(self):
+        """This is a function"""
+        raise Exception("area() is not implemented")
