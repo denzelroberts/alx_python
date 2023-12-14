89723c7a6f76b2534175a352c150eb1b67a5b246
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-14 16:00:42 +0000

    not making a difference

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,4 +1,7 @@
+class a_class:
+    pass
+
 """This is size of a function"""
 def inherits_from(obj, a_class):
-    """This is size of a function"""
+    """This is a function"""
     return issubclass(obj, a_class)
