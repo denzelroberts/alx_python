f4f18a891135c422b5f2ea9c7cb960bfc066baa8
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-14 16:23:16 +0000

    these things

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,4 +2,5 @@
 def inherits_from(obj, a_class):
     """This is a function"""
     # return issubclass(obj,a_class())
+    return issubclass(type(obj), a_class) and type(obj) != a_class
     return  type(obj) == a_class()
