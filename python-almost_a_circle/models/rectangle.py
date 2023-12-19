c92fa80cfe1454e0a7b62bd4bb9bb1bc30142a35
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-19 10:41:20 +0000

    getta and setta

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,34 +14,49 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ finds width """
+        """ checks width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ checks if width is a positive integer """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
     def height(self):
-        """ finds height """
+        """ checks height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ checks if height is a positive integer """
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
     def x(self):
-        """ finds x """
+        """ gets x """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ checks if x is a positive or zero integer """
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
     def y(self):
-        """ finds y """
+        """ gets y """
         return self.__y
 
     @y.setter
