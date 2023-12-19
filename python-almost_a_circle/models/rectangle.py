2604224444f8c46ba8bc6a6f7c8a41c18198884d
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-19 10:35:21 +0000

    rectangle_uno

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,3 +11,39 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """ finds width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """ finds height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """ finds x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """ finds y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
