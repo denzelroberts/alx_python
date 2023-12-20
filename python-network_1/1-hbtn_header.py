e0a7c5cfa573626c01478f6eb73bafa004fb8485
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-20 23:37:51 +0400

    task 1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,6 @@
+''' displays value of X-Request-Id found in header of response  '''
+import urllib.request as req
+from sys import argv
+if __name__ == "__main__":
+    with req.urlopen(req.Request(argv[1])) as response:
+        print(response.headers.get('X-Request-Id'))
