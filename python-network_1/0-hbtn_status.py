ee953136fc75be41aafe5d5d17496e7f9bd124b5
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-21 00:06:23 +0400

    updated

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,8 @@
-''' fetch from link '''
-import urllib.request as req
-if __name__ == "__main__":
-    with req.urlopen('https://intranet.hbtn.io/status') as res:
-        html = res.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+"""importing requests module"""
+import requests
+
+response = requests.get("https://alu-intranet.hbtn.io/status")
+
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
