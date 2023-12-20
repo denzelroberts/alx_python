bd58abe59d1dc9c8b009ac043b585d79b31b4567
denzelroberts <iamdenzelroberts@gmail.com>
2023-12-20 23:28:16 +0400

    task 0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+''' fetch from link '''
+import urllib.request as req
+if __name__ == "__main__":
+    with req.urlopen('https://intranet.hbtn.io/status') as res:
+        html = res.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
