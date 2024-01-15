49c1e8a25d279a7a26b19878b8e0239f02e22822
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-15 19:04:50 +0000

    b

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#import MySQLdb & sys
+import MySQLdb
+from sys import argv
+
+#connecting to mysqldb
+db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+#getting a cursor 
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+# "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+
+myresult = cur.fetchall()
+
+for state in myresult:
+  print(state)
+
+
+# Close all cursors
+cur.close()
+# Close all databases
+db.close()
