3e4d8c2577e021053baae4bf542bce029dc302dd
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-15 19:23:11 +0000

    single quote

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 #getting a cursor 
 cur = db.cursor()
 
-cur.execute("SELECT * FROM states WHERE name LIKE BINARY {} ORDER BY states.id ASC".format(argv[4]))
+cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4]))
 
 myresult = cur.fetchall()
 
