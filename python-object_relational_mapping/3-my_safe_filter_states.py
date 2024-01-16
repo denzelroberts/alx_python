77310402e9e1d09da2614af20ed2fcfe22c90f36
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-16 07:11:21 +0000

    comma

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 #getting a cursor 
 cur = db.cursor()
 
-cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC",(argv[4]))
+cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC",(argv[4],))
 
 myresult = cur.fetchall()
 
