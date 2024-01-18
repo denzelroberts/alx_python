6e841359703bec24693a328ee75aacbc0e02b8f2
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-18 17:13:26 +0000

    lets go

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,21 @@
 #import MySQLdb & sys & alchemy
-#import sqlalchemy
-import MySQLdb
 from sys import argv
-
-#connecting to mysqldb
-db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-
-#getting a cursor 
+import MySQLdb
+db = MySQLdb.connect(user=argv[1],
+                        passwd=argv[2],
+                        db=argv[3])
 cur = db.cursor()
-
 cur.execute("SELECT cities.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s ORDER BY cities.id",
-                (argv[4], ))
-
-myresult = cur.fetchall()
+            INNER JOIN states ON cities.state_id = states.id\
+            WHERE states.name = %s ORDER BY cities.id",
+            (argv[4], ))
+city_list = cur.fetchall()
 first = 0
-
-for city in myresult:
+for city in city_list:
     if first != 0:
         print(", ", end="")
-        print("%s" % city, end="")
-        first += 1
-
+    print("%s" % city, end="")
+    first += 1
 print("")
-# Close all cursors
 cur.close()
-# Close all databases
 db.close()
