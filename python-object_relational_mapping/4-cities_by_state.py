a4638d1ffb57912995b6a2f1e8736a34b17e2d60
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-18 10:37:50 +0000

    cities id

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 #getting a cursor 
 cur = db.cursor()
 
-cur.execute("SELECT * from cities ORDER BY states.id ASC")
+cur.execute("SELECT * from cities ORDER BY cities.id ASC")
 
 myresult = cur.fetchall()
 
