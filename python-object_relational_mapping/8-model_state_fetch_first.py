c4fc76d8beef80d502a27610389c2a42019af090
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-18 18:56:55 +0000

    CAM

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+'''importing sqlalhemy, sys, and model_state'''
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+path = "mysql+mysqldb://{}:{}@localhost/{}"
+engine = create_engine(path.format(argv[1], argv[2], argv[3]))
+
+Session = sessionmaker(bind = engine)
+session = Session()
+
+if session.query(State).order_by(State.id).all():
+    print("{}: {}".format(State.id, State.name))
+
+else:
+    print("Nothing")
+    
+session.close()
