5e0111d15420a82c2e43588196d12e6beab938e0
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-18 16:45:09 +0000

    for loop

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,30 @@
 #import MySQLdb & sys & alchemy
 #import sqlalchemy
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+import MySQLdb
+from sys import argv
 
-    #connecting to mysqldb
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+#connecting to mysqldb
+db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 
-    #getting a cursor 
-    cur = db.cursor()
+#getting a cursor 
+cur = db.cursor()
 
-    cur.execute("SELECT cities.name FROM cities\
-                    INNER JOIN states ON cities.state_id = states.id\
-                    WHERE states.name = %s ORDER BY cities.id",
-                    (argv[4], ))
+cur.execute("SELECT cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s ORDER BY cities.id",
+                (argv[4], ))
 
-    myresult = cur.fetchall()
-    first = 0
+myresult = cur.fetchall()
+first = 0
 
-    for city in myresult:
-        if first != 0:
-            print(", ", end="")
-            print("%s" % city, end="")
-            first += 1
+for city in myresult:
+    if first != 0:
+        print(", ", end="")
+print("%s" % city, end="")
+first += 1
 
-    print("")
-    # Close all cursors
-    cur.close()
-    # Close all databases
-    db.close()
+print("")
+# Close all cursors
+cur.close()
+# Close all databases
+db.close()
