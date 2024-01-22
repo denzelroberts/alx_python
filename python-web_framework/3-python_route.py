93f313a651561376fe8023a63e68c0e453cb49b2
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-22 19:19:01 +0000

    prefix

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -17,8 +17,8 @@ def ceee(text):
     text = escape(text).replace("_"," ")
     return "C {}".format(text)
 
-@app.route('/python/<is cool>',strict_slashes=False)
-def py(text):
+@app.route('/python/<text>',strict_slashes=False)
+def py(text = "is cool"):
     text = escape(text).replace("_"," ")
     return "Python {}".format(text)
 
