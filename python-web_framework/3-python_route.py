7ffbc6f4fab1a382304c415928e1800b7bef1ba4
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-22 19:39:33 +0000

    lets gooo

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -4,24 +4,23 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/',strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     return "Hello HBNB!"
 
-@app.route('/hbnb',strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
-@app.route('/c/<text>',strict_slashes=False)
+@app.route('/c/<text>')
 def ceee(text):
     text = escape(text).replace("_"," ")
     return "C {}".format(text)
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/')
+@app.route('/python/<text>')
 def py(text='is cool'):
-    text = escape(text).replace("_"," ")
-    return "Python {}".format(text)
+    return "Python {}".format(text.replace("_"," "))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", strict_slashes=False)
