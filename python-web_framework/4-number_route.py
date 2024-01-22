e3bb164e7993146d6610eddf86d8434f9f0184a4
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-22 19:55:56 +0000

    swear?

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -6,25 +6,30 @@ app = Flask(__name__)
 
 @app.route('/',strict_slashes=False)
 def hello_hbnb():
+    '''documented'''
     return "Hello HBNB!"
 
 @app.route('/hbnb',strict_slashes=False)
 def hbnb():
+    '''documented'''
     return "HBNB"
 
 @app.route('/c/<text>',strict_slashes=False)
 def ceee(text):
+    '''documented'''
     return "C {}".format(text.replace("_"," "))
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py(text='is cool'):
+    '''documented'''
     return "Python {}".format(text.replace("_"," "))
 
 '''documented'''
 @app.route('/number/', strict_slashes=False)
 @app.route('/number/<n>', strict_slashes=False)
 def py(n):
+    '''documented'''
     if n == int:
         return "{} is a number".format(n)
 
