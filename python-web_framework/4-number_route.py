d0d7728df3c1f58c16fcc7c033d3bc2eb83a888b
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-24 19:07:35 +0000

    check

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -16,16 +16,15 @@ def hbnb():
 def ceee(text):
     return "C {}".format(text.replace("_"," "))
 
-@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 def py(text='is cool'):
     return "Python {}".format(text.replace("_"," "))
 
-@app.route('/number/', strict_slashes=False)
 @app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/', strict_slashes=False)
 def number(n):
-    if n == int:
-        return "{} is a number".format(n)
+    return "{} is a number".format(n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
