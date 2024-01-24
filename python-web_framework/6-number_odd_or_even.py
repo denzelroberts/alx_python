24275d40afc2878a79e2dee930f30dd8f8c7a10b
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-24 19:42:07 +0000

    hmmm

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -24,17 +24,17 @@ def py(text='is cool'):
 def number(n):
     return "{} is a number".format(n)
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def number_template(n):
     return render_template('5-number.html', number=n)
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>')
 def even_or_odd(n):
-    if n % 2 == 0:
-        return render_template('5-number.html', number=n, even_or_odd = 'even')
+    if (n % 2) == 0:
+        return render_template('6-number_odd_or_even.html', number=n, even_or_odd = 'even')
     
     else:
-        return render_template('5-number.html', number=n, even_or_odd = 'odd') 
+        return render_template('6-number_odd_or_even.html', number=n, even_or_odd = 'odd') 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
