a715f78f4facdf768fee118c9d2757e37f702def
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-24 18:53:52 +0000

    int

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -1,4 +1,5 @@
-"""raghhman"""
+#!/usr/bin/python3
+'''Importing Flask form flask'''
 from flask import Flask
 from markupsafe import escape
 
@@ -6,33 +7,26 @@ app = Flask(__name__)
 
 @app.route('/',strict_slashes=False)
 def hello_hbnb():
-    '''documented'''
     return "Hello HBNB!"
 
 @app.route('/hbnb',strict_slashes=False)
 def hbnb():
-    '''documented'''
     return "HBNB"
 
 @app.route('/c/<text>',strict_slashes=False)
 def ceee(text):
-    '''documented'''
     return "C {}".format(text.replace("_"," "))
 
-@app.route('/python/', strict_slashes=False)
+#@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py(text='is cool'):
-    '''documented'''
     return "Python {}".format(text.replace("_"," "))
 
-'''documented'''
 @app.route('/number/', strict_slashes=False)
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def py(n):
-    '''documented'''
     if n == int:
         return "{} is a number".format(n)
 
-'''documented'''
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
