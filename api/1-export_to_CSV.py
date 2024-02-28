4496d86680538ac300ba236df5cf1993cc068363
denzelroberts <iamdenzelroberts@gmail.com>
2024-02-28 12:25:39 +0000

    RASSSS

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -43,9 +43,9 @@ def get_employee_todo_progress(employee_id):
         sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    # if len(sys.argv) != 2:
+    #     print("Usage: python script.py <employee_id>")
+    #     sys.exit(1)
 
     employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
