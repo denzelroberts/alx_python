8c0084600eefe1a9589ba31d620862822298e1f1
denzelroberts <iamdenzelroberts@gmail.com>
2024-03-01 08:44:11 +0000

    dictionary

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,39 +1,35 @@
-#!/usr/bin/python3
-
-"""
-Python script that exports data in the JSON format.
-"""
-
-from requests import get
+"""import libraries alphabetically"""
 import json
+import requests
+
+"""creating function"""
+def getData():
+    """
+    Data from json API
+    """
+    usersurl = "https://jsonplaceholder.typicode.com/users"
+
+    request1 = requests.get(usersurl)
+    results = request1.json()
+
+    alldata = {}
+
+    for result in results:
+        username = result['username']
+        userid = result['id']
+        todourl = "https://jsonplaceholder.typicode.com/users/{}/todos".format(userid)
+        request2 = requests.get(todourl)
+        tasks = request2.json()
+        jsondata = [
+                {"username": username, "task": task['title'], "completed": task['completed']}
+                for task in tasks
+            ]
+        
+        alldata[str(userid)] = jsondata
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(alldata, jsonfile)
+
 
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-
-    row = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
-
-    new_dict1 = {}
-
-    for j in data2:
-
-        row = []
-        for i in data:
-
-            new_dict2 = {}
-
-            if j['id'] == i['userId']:
-
-                new_dict2['username'] = j['username']
-                new_dict2['task'] = i['title']
-                new_dict2['completed'] = i['completed']
-                row.append(new_dict2)
-
-        new_dict1[j['id']] = row
-
-    with open("todo_all_employees.json",  "w") as f:
-
-        json_obj = json.dumps(new_dict1)
-        f.write(json_obj)
+    getData()
